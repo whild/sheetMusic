c22a29e10ee5ac4f9deff18a5eb1bddae3d4af63
SeoDongjoo <djseo@pisoft.co.kr>
2023-02-03 16:56:50 +0900

    이동 플랫폼 제작 완료

---
+++ Assets/Sprites/Object/MovingPlatform.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] Transform start;
+    [SerializeField] Transform end;
+    [SerializeField] float Duration;
+
+    [SerializeField] Vector3 startpos;
+    [SerializeField] Vector3 endpos;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (start == null || end == null)
+        {
+            Debug.LogError("There is no Pos");
+            return;
+        }
+        startpos = start.position;
+        endpos = end.position;
+
+        this.transform.position = startpos;
+
+        this.transform
+            .DOMove(endpos, Duration)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+
+
+}
