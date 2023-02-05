2d7924bb91df31664ffd7e7b14034f5170b72759
개발 서동주 <tjehdwn9924@gmail.com>
2023-02-05 21:39:41 +0900

    사용하던 어트리뷰트 패키지 임포트, 카메라 조작 기능 추가 (특정 오브젝트 1회 집중, 특정 거리 안에 있으면 계같이 비추기), 스크립트 폴더 말고 스프라이트 폴더에 스크립트 만들던거 폴더 변경

---
+++ Assets/Scripts/Cinemachine/CameraMagnetProperty.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMagnetProperty : FocusObject
+{
+    [Range(0.1f, 50.0f)]
+    public float MagnetStrength = 5.0f;
+    
+    [Range(0.1f, 50.0f)]
+    public float Proximity = 5.0f;
+
+    public Transform ProximityVisualization;
+
+    [HideInInspector] public Transform myTransform;
+
+    void Start()
+    {
+        myTransform = transform;
+        if (ProximityVisualization != null)
+            ProximityVisualization.localScale = new Vector3(Proximity * 2.0f, Proximity * 2.0f, 1);
+
+        SphereCollider collider = this.gameObject.AddComponent<SphereCollider>();
+        collider.radius = Proximity * 2.0f;
+        collider.isTrigger = true;
+    }
+}
---
+++ Assets/Scripts/Cinemachine/CameraMagnetTargetController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraMagnetTargetController : Manager<CameraMagnetTargetController>
+{
+    public CinemachineTargetGroup targetGroup;
+
+    private int playerIndex;
+    private FocusObject[] cameraMagnets = new FocusObject[] { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerIndex = 0;
+    }
+
+    public void AddTargetGruop(Transform trans, FocusType focusType)
+    {
+        Array.Resize(ref targetGroup.m_Targets, targetGroup.m_Targets.Length + 1);
+        targetGroup.m_Targets[targetGroup.m_Targets.Length - 1].target = trans;
+
+        FocusEffect(trans, focusType);
+
+    }
+
+    public void DeleteTargetGruop(Transform trans)
+    {
+        var container = targetGroup.m_Targets.ToList();
+        container.Remove(container.Find(val => val.target == trans));
+        targetGroup.m_Targets = container.ToArray();
+        targetGroup.m_Targets[playerIndex].weight = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetGroup.m_Targets[playerIndex].weight != 0 && cameraMagnets.Length >= 0)
+        {
+            for (int i = 1; i < targetGroup.m_Targets.Length; ++i)
+            {
+                float distance = (targetGroup.m_Targets[playerIndex].target.position - targetGroup.m_Targets[i].target.position).magnitude;
+                if (distance < cameraMagnets[i - 1].Proximity)
+                {
+                    targetGroup.m_Targets[i].weight = cameraMagnets[i - 1].MagnetStrength * (1 - (distance / cameraMagnets[i - 1].Proximity));
+                }
+                else
+                {
+                    targetGroup.m_Targets[i].weight = 0;
+                }
+            }
+        }
+    }
+
+    private void FocusEffect(Transform target, FocusType focusType)
+    {
+        switch (focusType)
+        {
+            case FocusType.Together:
+                Together(target);
+                break;
+            case FocusType.Once:
+                Focus(target);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Together(Transform target)
+    {
+        var container = (cameraMagnets.Length != 0) ? cameraMagnets.ToList() : new List<FocusObject>();
+        container.Add(target.GetComponent<FocusObject>());
+        this.cameraMagnets = container.ToArray();
+    }
+
+    private void Focus(Transform target)
+    {
+        for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+        {
+            targetGroup.m_Targets[i].weight = 0;
+            if(targetGroup.m_Targets[i].target == target)
+            {
+                targetGroup.m_Targets[i].weight = 1;
+            }
+        }
+    }
+}
