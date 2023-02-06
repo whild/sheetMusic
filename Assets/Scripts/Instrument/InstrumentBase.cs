d4b965e2d704abeabbbd037501f64c0cb109800a
개발 서동주 <tjehdwn9924@gmail.com>
2023-02-06 22:17:15 +0900

    악기 베이스 스크립트 추가

---
+++ Assets/Scripts/Instrument/InstrumentBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum instrument 
+{
+    Automaton = 0,
+    Violin = 1,
+    Horn = 2,
+}
+
+
+public class InstrumentBase : MonoBehaviour
+{
+
+}
