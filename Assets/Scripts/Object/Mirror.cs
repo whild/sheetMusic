d05625dd8a7c4f9cbf4e515a152952ca2fcfd258
SeoDongjoo <djseo@pisoft.co.kr>
2023-02-15 11:48:05 +0900

    레이저 작엽 시작

---
+++ Assets/Scripts/Object/Mirror.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.gameObject.tag = "Mirror";
+    }
+}
