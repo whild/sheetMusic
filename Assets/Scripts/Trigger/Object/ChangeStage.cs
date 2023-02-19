4ade6133db55f71808bc74400ed73d07bf2cf432
개발 서동주 <tjehdwn9924@gmail.com>
2023-02-19 17:19:34 +0900

    no message

--- Assets/Scripts/Trigger/Object/ChangeStage.cs
+++ Assets/Scripts/Trigger/Object/ChangeStage.cs
@@ -7,12 +7,42 @@ public class ChangeStage : TriggerInteractCore
     [SerializeField] StageDataBase nextStageBase;
     [SerializeField] bool needKey;
 
+    public static bool goal3D;
+    public static bool goal2D;
+    public static bool haveKey;
+
     protected override void Awake()
     {
         base.Awake();
     }
 
+    public static void ReData(bool d3, bool d2, bool key)
+    {
+        goal3D = !d3;
+        goal2D = !d2;
+        haveKey = !key;
+    }
+
     public override void OnTrigger(Collider collision)
+    {
+        CheckKey();
+        if (collision.CompareTag(TagManager.player))
+        {
+            goal3D = true;
+        }
+        CheckNextStage();
+    }
+
+    public override void OnTrigger(Collider2D collision)
+    {
+        CheckKey();
+        if (collision.CompareTag(TagManager.player))
+        {
+            goal2D = true;
+        }
+    }
+
+    private void CheckKey()
     {
         if (needKey)
         {
@@ -21,7 +51,16 @@ public class ChangeStage : TriggerInteractCore
                 Debug.Log("This stage need Key for go to next stage. but you don't have key!");
                 return;
             }
+            haveKey = true;
         }
-        StageManager.Instance.NextStage(this.nextStageBase);
+        CheckNextStage();
+    }
+
+    private void CheckNextStage()
+    {
+        if (goal3D && goal2D && haveKey)
+        {
+            StageManager.Instance.NextStage(this.nextStageBase);
+        }
     }
 }
