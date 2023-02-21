94fe349fa820c6b6fcf28401b98f2354c96082e5
개발 서동주 <tjehdwn9924@gmail.com>
2023-02-21 23:12:51 +0900

    몸 비벼서 깨지는 현상 수정

--- Assets/Scripts/PlayerAct/ReactObject/BreakPoint.cs
+++ Assets/Scripts/PlayerAct/ReactObject/BreakPoint.cs
@@ -8,20 +8,42 @@ public class BreakPoint : MonoBehaviour, IPlayerReact
     public int syncroIndex;
     private List<React_Break> reacts = new List<React_Break>();
 
+    private bool isPlayeract;
 
     private void Start()
     {
+        isPlayeract = false;
         reacts = SyncroOjbect.GetSyncroObjects<React_Break>().ToList();
     }
 
     public void PlayerReact(int instrumentValue)
     {
-        foreach (var react in reacts)
+        if (isPlayeract)
         {
-            if (react.syncroIndex == this.syncroIndex)
+            foreach (var react in reacts)
             {
-                react.PlayerReact(instrumentValue);
+                if (react.syncroIndex == this.syncroIndex)
+                {
+                    react.PlayerReact(instrumentValue);
+                }
             }
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == other.GetComponent<SphereCollider>())
+        {
+            isPlayeract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other == other.GetComponent<SphereCollider>())
+        {
+            isPlayeract = false;
+        }
+    }
+
 }
